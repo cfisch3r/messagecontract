ea08af439ca1efd6bc40e8b7364c3c01285e4da4
Christian Fischer <cf@fisch3r.de>
2017-02-25 13:24:09 +0100

    initial import

---
+++ src/test/java/de/agiledojo/messagecontract/MessageClientTest.java
@@ -0,0 +1,12 @@
+package de.agiledojo.messagecontract;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class MessageClientTest {
+
+    @Test
+    public void testWorks() {
+        Assertions.assertThat(true).isFalse();
+    }
+}
