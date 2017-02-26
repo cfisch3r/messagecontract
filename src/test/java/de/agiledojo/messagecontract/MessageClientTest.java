371a0772e245912b1eb0d5d64c5ddeaddef13440
Christian Fischer <cf@fisch3r.de>
2017-02-26 08:40:07 +0100

    send message

--- src/test/java/de/agiledojo/messagecontract/MessageClientTest.java
+++ src/test/java/de/agiledojo/messagecontract/MessageClientTest.java
@@ -4,10 +4,12 @@ import com.github.geowarin.junit.DockerRule;
 import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.util.Properties;
 
@@ -29,6 +31,41 @@ public class MessageClientTest {
         Properties queueProperties = admin.getQueueProperties("myQueue");
         Assertions.assertThat(queueProperties).isNotNull();
     }
+
+    @Test
+    public void canSendMessage() {
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory());
+        Queue queue = new Queue("myQueue");
+        admin.declareQueue(queue);
+        TopicExchange exchange = new TopicExchange("myExchange");
+        admin.declareExchange(exchange);
+        admin.declareBinding(
+                BindingBuilder.bind(queue).to(exchange).with("foo.*"));
+
+        RabbitTemplate rabbitClient = new RabbitTemplate(connectionFactory());
+        rabbitClient.convertAndSend("myExchange","foo.bar","my message");
+        rabbitClient.setQueue("myQueue");
+        Object message = rabbitClient.receiveAndConvert();
+        Assertions.assertThat(message).isNotNull();
+    }
+
+    //    @Test
+//    public void canSendAndReceiveMessage() {
+//        RabbitAdmin admin = new RabbitAdmin(connectionFactory());
+//        admin.declareQueue(new Queue("myQueue"));
+//        RabbitTemplate rabbit = new RabbitTemplate(connectionFactory());
+//
+//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//        container.addQueueNames("myQueue");
+//        container.setConnectionFactory(connectionFactory());
+//        final StringBuilder body = new StringBuilder();
+//        container.setMessageListener(new MessageListener() {
+//            public void onMessage(Message message) {
+//                body.append(message.getBody());
+//            }
+//        });
+//        Assertions.assertThat(body).isNotEmpty();
+//    }
 
     private CachingConnectionFactory connectionFactory() {
         CachingConnectionFactory cf = new CachingConnectionFactory();
